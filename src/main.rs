eb8b6039d2bd95a16c9c861517397c2a237773e5
Damian Cyntler <damian@cyntler.com>
2022-07-31 10:38:12 +0200

    Display final value

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-mod currency_api;
-use crate::currency_api::fetch_currencies;
+use currency_converter::{args_reader, currency_api};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let currencies = fetch_currencies().await?;
+    let (from_currency, to_currency, amount) = args_reader::read();
+    let current_currencies_value =
+        currency_api::currencies_value(&from_currency, &to_currency).await?;
 
-    println!("{:#?}", currencies);
+    let amount_float = amount.parse::<f64>().unwrap();
+    let currency_value = current_currencies_value[&to_currency].as_f64().unwrap();
+
+    println!(
+        "{:.2} {}",
+        amount_float * currency_value,
+        to_currency.to_uppercase()
+    );
 
     Ok(())
 }
