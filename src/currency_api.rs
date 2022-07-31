0a38c29f92b6155755aa6e4fa1dc0c9402bf322b
Damian Cyntler <damian@cyntler.com>
2022-07-31 09:44:11 +0200

    add currencies_value endpoint to api

--- src/currency_api.rs
+++ src/currency_api.rs
@@ -1,13 +1,17 @@
-use std::collections::HashMap;
 use std::error::Error;
 
 const CURRENCY_API_URL: &str = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest";
 
-pub async fn fetch_currencies() -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let response = reqwest::get(format!("{CURRENCY_API_URL}/currencies.min.json"))
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
+pub async fn currencies_value(
+    from_currency: &String,
+    to_currency: &String,
+) -> Result<serde_json::Value, Box<dyn Error>> {
+    let response = reqwest::get(format!(
+        "{CURRENCY_API_URL}/currencies/{from_currency}/{to_currency}.min.json"
+    ))
+    .await?
+    .json::<serde_json::Value>()
+    .await?;
 
     Ok(response)
 }
